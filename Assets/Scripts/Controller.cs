c4d827179a8a3e51846e9862151d49f754a773cd
Andrii <ABilyk@uclan.ac.uk>
2021-03-08 15:09:52 +0200

    UI update
    very pog

---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    public TextMeshProUGUI mTitle;
+    public GameObject mPanleTitle;
+    public GameObject mStats;
+
+    private float width;
+    private float height;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        height = Screen.height;
+        width = Screen.width;
+
+        mTitle.SetText("Level: " + height +" | "+ width);
+
+
+        mPanleTitle.transform.position = new Vector2(Screen.width / 2.0f, Screen.height -150f);
+        mStats.transform.position = new Vector2(Screen.width / 2.0f, Screen.height /6f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    
+}
--- Assets/Scripts/UIScale.cs
+++ Assets/Scripts/UIScale.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameContriller : MonoBehaviour
+public class UIScale : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
