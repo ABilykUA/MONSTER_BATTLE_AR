7c97d10c152e619dc18249f18f4d0efbf8a186ac
EIoannou42 <eioannou4@uclan.ac.uk>
2021-03-12 16:45:15 +0200

    Abilities class

---
+++ Assets/Scripts/Abilities.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Abilities
+{
+    public int damage { get; set; }
+    public int heal { get; set; }
+    public string type { get; set; }
+    public int uses { get; set; }
+
+    Abilities(int damage, string type,int heal,int uses)
+    {
+        this.damage = damage;
+        this.type = type;
+        this.heal = heal;
+        this.uses = uses;
+    }
+
+}
