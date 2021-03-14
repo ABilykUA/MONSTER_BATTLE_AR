03bec4aae53db72222d0a89b7d18a6ca59610b98
AIvanov352 <AIvanov@uclan.ac.uk>
2021-03-14 14:04:44 +0200

    Made abilities function and damage calculation

--- Assets/Scripts/Abilities.cs
+++ Assets/Scripts/Abilities.cs
@@ -4,13 +4,15 @@ using UnityEngine;
 
 public class Abilities
 {
+    public string name { get; set; }
     public int damage { get; set; }
     public int heal { get; set; }
     public string type { get; set; }
     public int uses { get; set; }
 
-    Abilities(int damage, string type,int heal,int uses)
+    Abilities(string name, int damage, string type,int heal,int uses)
     {
+        this.name = name;
         this.damage = damage;
         this.type = type;
         this.heal = heal;
