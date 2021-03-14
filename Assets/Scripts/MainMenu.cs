8d4ea6a082c437b025e565ec2183e4a192cc975e
EIoannou42 <eioannou4@uclan.ac.uk>
2021-03-14 15:18:08 +0200

    Scalable text

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -2,15 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class MainMenu : MonoBehaviour
 {
+    public TextMeshProUGUI scaleTutorial;
+    public TextMeshProUGUI MainMenuTitle;
+    public TextMeshProUGUI PlayButtonText;
+    public TextMeshProUGUI ExitButtonText;
+    public TextMeshProUGUI DisabilityTitle;
+    public TextMeshProUGUI ColorBlindText;
+    public TextMeshProUGUI BlurredVisionText;
+    public TextMeshProUGUI NormalVisionText;
+    public TextMeshProUGUI BackButtonText;
 
+    public void TextSize(float fontSize)
+    {
+        scaleTutorial.fontSize = fontSize+30;
+        MainMenuTitle.fontSize = fontSize+30;
+        PlayButtonText.fontSize = fontSize;
+        ExitButtonText.fontSize = fontSize;
+        DisabilityTitle.fontSize = fontSize+30;
+        ColorBlindText.fontSize = fontSize;
+        BlurredVisionText.fontSize = fontSize;
+        NormalVisionText.fontSize = fontSize;
+        BackButtonText.fontSize = fontSize;
+
+    }
     public void PlayGame() {
 
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
   
     }
+
 
     public void ExitGame()
     {
