fb19757f5ae3b687d938921bde32edfbfe163e89
ABILYKUA <ABilyk@uclan.ac.uk>
2021-04-02 14:41:26 +0300

    ui overhaul

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -7,26 +7,9 @@ using UnityEngine;
     public class Controller : MonoBehaviour
     {
        
-        public GameObject mPanleTitle;
-        public GameObject mUI;
-
+   
         public GameObject EnemyUI;
         public GameObject ARcam;
-
-        private float width;
-        private float height;
-
-        // Start is called before the first frame update
-        void Start()
-        {
-            height = Screen.height;
-            width = Screen.width;
-
-         mPanleTitle.transform.position = new Vector2(Screen.width / 2.0f, Screen.height - 150f);
-             mUI.transform.position = new Vector2(Screen.width / 2.0f, Screen.height / 6f);
-
-
-        }
 
         // Update is called once per frame
         void Update()
@@ -35,6 +18,6 @@ using UnityEngine;
         EnemyUI.transform.LookAt(ARcam.transform.position);
         EnemyUI.transform.Rotate(-90, 180, 0);
         //EnemyUI.transform.LookAt(ARcam.transform);
-    }
+        }
 
     }
